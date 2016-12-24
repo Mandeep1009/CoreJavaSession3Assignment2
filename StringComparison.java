7c460aa0f6b93dff0f1c2e05f1757003f437fee6
Mandeep1009 <yadavmandeep4@gmail.com>
2016-12-24 21:20:37 +0530

    Add files via upload

---
+++ StringComparison.java
@@ -0,0 +1,17 @@
+class StringComparison
+{
+	public static void main(String args[])
+	{
+		String s1 = "Mandeep"; 		// variable S1 of data type string
+		String s2 = "mandeep"; 		// variable S1 of data type string
+		boolean same; 		 	// variable Same of data type boolean
+		if (same = s1==s2)		
+		{
+			System.out.println("Value of the Boolean Data Type same is : " + same);
+		}
+		else
+		{
+			System.out.println("Value of the Boolean Data Type same is : " + same);
+		}
+	}
+}
